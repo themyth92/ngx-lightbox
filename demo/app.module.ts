a2f761626d92c6d69b39a63b1e8e1bf2eb76d708
paul <themyth92@gmail.com>
2019-07-19 11:59:11 +0800

    [#23] Fix problem with disable scrolling not working
    Fix #23

--- demo/app.module.ts
+++ demo/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+
 import { LightboxModule } from '../src';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [ BrowserModule, LightboxModule ],

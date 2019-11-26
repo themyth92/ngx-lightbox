e614d10e12cddf53c7e2991ef16e22c31b11b328
Tom Fitzgerald <tomscript@users.noreply.github.com>
2019-11-26 15:55:02 -0800

    porting fix from angular2-lightbox
    https://github.com/themyth92/angular2-lightbox/commit/9c2c9dd7a9dec594fe40e47d49ec5a32d3ac0295
    https://github.com/themyth92/angular2-lightbox/pull/39#event-1988717202
    
    disclaimer: i did not actually test this

--- src/lightbox.component.ts
+++ src/lightbox.component.ts
@@ -291,7 +291,8 @@ export class LightboxComponent implements OnInit, AfterViewInit, OnDestroy, OnIn
     const newHeight = imageHeight + this._cssValue.containerTopPadding + this._cssValue.containerBottomPadding +
       this._cssValue.imageBorderWidthTop + this._cssValue.imageBorderWidthBottom;
 
-    if (oldWidth !== newWidth || oldHeight !== newHeight) {
+    // make sure that distances are large enough for transitionend event to be fired, at least 5px.
+    if (Math.abs(oldWidth - newWidth) + Math.abs(oldHeight - newHeight) > 5) {
       this._rendererRef.setStyle(this._outerContainerElem.nativeElement, 'width', `${newWidth}px`);
       this._rendererRef.setStyle(this._outerContainerElem.nativeElement, 'height', `${newHeight}px`);
 

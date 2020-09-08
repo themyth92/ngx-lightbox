ac63e22b365c303e83580599b95f8d159c32361a
digiconstat <digiconstat@pop-os.localdomain>
2020-09-08 16:00:11 +0100

    bugfix : changed config showRotation to showRotate

--- src/lightbox-config.service.ts
+++ src/lightbox-config.service.ts
@@ -15,7 +15,7 @@ export class LightboxConfig {
   public enableTransition: boolean;
   public albumLabel: string;
   public showZoom: boolean;
-  public showRotation: boolean;
+  public showRotate: boolean;
   constructor() {
     this.fadeDuration = 0.7;
     this.resizeDuration = 0.5;
@@ -30,6 +30,6 @@ export class LightboxConfig {
     this.enableTransition = true;
     this.albumLabel = 'Image %1 of %2';
     this.showZoom = false;
-    this.showRotation = false;
+    this.showRotate = false;
   }
 }
